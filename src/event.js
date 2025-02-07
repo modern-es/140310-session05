549d6652271dd866b6146ce0a11f2a4b1d6a92d7
Pashaie <pashaie@gmail.com>
2025-02-07 09:49:24 +0330

    events

---
+++ src/event.js
@@ -0,0 +1,26 @@
+import confetti from 'canvas-confetti';
+
+const button = document.querySelector('#events button');
+button.addEventListener("click", e => {
+    confetti({
+        particleCount: 100,
+        spread: 70,
+        origin: { y: 0.6 }
+    });
+});
+
+
+const input = document.querySelector('#events input');
+input.addEventListener('keypress', e => {
+    if (e.key >= '0' && e.key <= '9') {
+        //
+    } else {
+        e.preventDefault();
+    }
+})
+//keydown
+//keypress
+
+//input/change
+//keyup
+
