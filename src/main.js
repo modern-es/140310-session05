508488c9b4d4f75ec540a06deb7c81521f67c062
Pashaie <pashaie@gmail.com>
2025-02-07 12:39:27 +0330

    ..

--- src/main.js
+++ src/main.js
@@ -1,13 +1,29 @@
 import '@picocss/pico/css/pico.min.css'
 import { qs } from './utils/dom'
+
+//CORS
+/*
+    Domain -> host name + port
+        front: http://localhost:5174
+        front: http://127.0.0.1:5174
+
+        back:  http://127.0.0.1:8090
+
+        fad.ir
+        fad.ir:9090
+
+    - Backend bayad CORS ro handle konad
+
+ */
+
+
 async function loadStudents() {
     const resp = await fetch('http://127.0.0.1:8090' + '/api/collections/students/records', {
         method: 'GET'
     });
     const data = await resp.json();
     const tbody = qs('tbody');
-    console.log(tbody)
-
+    //Tamrin: handlebars
     let result = '';
     for (const item of data.items) {
         result += `<tr>
@@ -15,10 +31,34 @@ async function loadStudents() {
             <td>${item.firstName}</td>
             <td>${item.lastName}</td>
             <td>${item.isActive}</td>
-            <td>edit, delete</td>
+            <td>edit, 
+                <button onClick="deleteStudent('${item.id}')">❌</button>
+                <button onClick="toggleStudent('${item.id}', ${item.isActive})">☀️</button>
+            </td>
         </tr>`
     }
     tbody.innerHTML = result;
+}
+
+globalThis.toggleStudent = async (id, active) => {
+    await fetch('http://127.0.0.1:8090' + '/api/collections/students/records/' + id, {
+        method: 'PATCH',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            isActive: !active
+        })
+    });
+    loadStudents();
+}
+globalThis.deleteStudent = async (id) => {
+    if (confirm('are you sure?')) {
+        await fetch('http://127.0.0.1:8090' + '/api/collections/students/records/' + id, {
+            method: 'DELETE'
+        });
+        loadStudents();
+    }
 }
 
 loadStudents();
@@ -38,3 +78,5 @@ qs('#addStudent').addEventListener('click', async e => {
 
     loadStudents();
 })
+
+
